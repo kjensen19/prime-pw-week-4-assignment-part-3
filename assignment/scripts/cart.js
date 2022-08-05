116403b8f762cc336b95446dc5a755031e02191d
Kyle Jensen <kjensen19@gmail.com>
2022-08-05 00:27:40 -0500

    cleaning up if/else formatting

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,7 +7,7 @@ let basket = [];
 // 1. Add a global `const` named `maxItems` and set it to 5.
 const maxItems = 5;
 
-console.log('Does basket exist, expect empty', basket)
+console.log('Does basket exist, expect empty', basket);
 console.log('Should be 5', maxItems);
 
 // - Create a function called `addItem`. It should:
@@ -25,30 +25,29 @@ function addItem(cont, item) {
     } else {
         return false;
     }
-}
+};
 // - Create a function called `listItems`. It should:
 //   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
-function listItems(){
-    for (let item of basket) {
+function listItems(cont) {
+    for (let item of cont) {
         console.log(item);
     }
-}
+};
 // - Create a function called `empty`. It should:
 //   - reset the `basket` to an empty array
-function empty(cont){
+function empty(cont) {
     cont.splice(0, cont.length);
     console.log('in empty, should zero arr', cont)
     console.log('basket in empty', basket);
-}
+};
 // 2. Create a function called isFull(). It should:
 //   - return `false` if the basket contains *less* than max number of items
 //   - return `true` otherwise (equal or more than maxItems)
-function isFull(cont){
+function isFull(cont) {
     if (cont.length < maxItems) {
         return false;
-    }
-    else{
+    } else {
         return true
     }
 };
@@ -60,7 +59,7 @@ function removeItem(cont, item){
     } else {
         return cont.splice(remIndex, 1)
     }
-}
+};
 
 
 
@@ -87,3 +86,5 @@ console.log(`basket = ${basket}`);
 console.log(`removed item = ${removeItem(basket, 'pear')}`);
 console.log(`removed item = ${removeItem(basket, 'goat')}`);
 console.log(`basket = ${basket}`);
+console.log('should list out items below');
+listItems(basket);
